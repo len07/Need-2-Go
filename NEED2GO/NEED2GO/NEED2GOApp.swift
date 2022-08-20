4ba5bd6cad2bcff29673914a094110f33608c2f7
len07 <83131630+len07@users.noreply.github.com>
2022-08-20 16:50:00 -0400

    Add files via upload

---
+++ NEED2GO/NEED2GO/NEED2GOApp.swift
@@ -0,0 +1,17 @@
+//
+//  NEED2GOApp.swift
+//  NEED2GO
+//
+//  Created by Alena Luo on 2022-08-19.
+//
+
+import SwiftUI
+
+@main
+struct NEED2GOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
