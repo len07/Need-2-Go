a891c85d37133af1c3e07aa6b5b1140fd90356e1
Amy Uccello <amy.sandar.uccello@gmail.com>
2022-08-19 23:30:50 -0400

    create part of crud app as well as improvements to front end website

--- reviews/urls.py
+++ reviews/urls.py
@@ -6,8 +6,9 @@ urlpatterns = [
     re_path(r'^$', views.review_list, name='review_list'),
     # ex: /review/5/
     re_path(r'^review/(?P<review_id>[0-9]+)/$', views.review_detail, name='review_detail'),
-    # ex: /wine/
+    # ex: /bathroom/
     re_path(r'^bathroom$', views.bathroom_list, name='bathroom_list'),
-    # ex: /wine/5/
+    # ex: /bathroom/5/
     re_path(r'^bathroom/(?P<bathroom_id>[0-9]+)/$', views.bathroom_detail, name='bathroom_detail'),
+    re_path(r'^bathroom/(?P<bathroom_id>[0-9]+)/add_review/$', views.add_review, name='add_review'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -4,6 +4,14 @@ from django.shortcuts import get_object_or_404, render
 
 from .models import Bathroom, Review
 
+from .forms import ReviewForm
+
+from django.http import HttpResponseRedirect
+
+from django.urls import reverse
+
+import datetime
+
 
 def review_list(request):
     latest_review_list = Review.objects.order_by('-pub_date')[:9]
@@ -24,4 +32,23 @@ def bathroom_list(request):
 
 def bathroom_detail(request, bathroom_id):
     bathroom = get_object_or_404(Bathroom, pk=bathroom_id)
-    return render(request, 'reviews/bathroom_detail.html', {'bathroom': bathroom})
+    form = ReviewForm()
+    return render(request, 'reviews/bathroom_detail.html', {'bathroom': bathroom, 'form':form})
+
+def add_review(request, bathroom_id):
+    bathroom = get_object_or_404(Bathroom, pk=bathroom_id)
+    form = ReviewForm(request.POST)
+    if form.is_valid():
+        rating = form.cleaned_data['rating']
+        comment = form.cleaned_data['comment']
+        user_name = form.cleaned_data['user_name']
+        review = Review()
+        review.bathroom = bathroom
+        review.user_name = user_name
+        review.rating = rating
+        review.comment = comment
+        review.pub_date = datetime.datetime.now()
+        review.save()
+        return HttpResponseRedirect(reverse('reviews:bathroom_detail', args=(bathroom.id,)))
+
+    return render(request, 'reviews/bathroom_detail.html', {'bathroom': bathroom, 'form': form})
